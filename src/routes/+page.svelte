fd06da8c95513794c148529a90d451cfe4e9ed76
Marty Penner <martypenner@users.noreply.github.com>
2023-10-11 21:58:53 -0400

    feat: increase scrub button size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -66,7 +66,7 @@
 	<button
 		type="submit"
 		form="form"
-		class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+		class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
 		disabled={(dataTransfer?.files ?? []).length === 0}
 	>
 		Scrub
